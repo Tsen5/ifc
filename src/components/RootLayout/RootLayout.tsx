0b9f120c70e8d1f2c33865059856ad01b1ccaa43
Swann LEGROS <swann.legros@dametis.com>
2025-03-23 00:29:11 +0100

    feat: adding emotion and reset css

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -1,8 +1,15 @@
+import { css, Global } from '@emotion/react';
 import { Outlet } from '@tanstack/react-router';
+import emotionReset from 'emotion-reset';
 
 function RootLayout() {
   return (
     <>
+      <Global
+        styles={css`
+          ${emotionReset}
+        `}
+      />
       <Outlet />
     </>
   );
