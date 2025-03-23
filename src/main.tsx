5c7e6a3c79a22b0ead646f7e123a3fe6994d7633
Swann LEGROS <swann.legros@dametis.com>
2025-03-23 15:34:33 +0100

    feat: adding radix

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import { Theme } from '@radix-ui/themes';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { routeTree } from './routeTree.gen';
+
+import '@radix-ui/themes/styles.css';
 
 const router = createRouter({ routeTree, defaultPreload: 'intent' });
 
@@ -17,7 +20,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <Theme accentColor="ruby" grayColor="mauve" radius="large" appearance="dark">
+        <RouterProvider router={router} />
+      </Theme>
     </StrictMode>
   );
 }
