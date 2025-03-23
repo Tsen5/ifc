769572ec35f5978f156b0d79cac74dde2d087ea3
Swann LEGROS <swann.legros@dametis.com>
2025-03-23 15:35:58 +0100

    fix: missing en locale

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import en from './en';
 import fr from './fr';
 
 // import LanguageDetector from 'i18next-browser-languagedetector';
 
 export const resources = {
   fr,
+  en,
 } as const;
 
 export type DefaultNS = keyof typeof fr;
