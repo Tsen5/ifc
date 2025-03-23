c20ea9c5e07a0ca945221eb00f69f1e9b3ec6b0c
Swann LEGROS <swann.legros@dametis.com>
2025-03-23 19:47:09 +0100

    feat(i18n): adding days names

--- src/i18n/en/calendar.ts
+++ src/i18n/en/calendar.ts
@@ -14,4 +14,31 @@ export default {
     month_12: 'November',
     month_13: 'December',
   },
+  tooltip: {
+    previousMonth: 'Previous month',
+    nextMonth: 'Next month',
+  },
+  button: {
+    today: 'Today',
+  },
+  days: {
+    short: {
+      day_1: 'Sun',
+      day_2: 'Mon',
+      day_3: 'Tue',
+      day_4: 'Wed',
+      day_5: 'Thu',
+      day_6: 'Fri',
+      day_7: 'Sat',
+    },
+    long: {
+      day_1: 'Sunday',
+      day_2: 'Monday',
+      day_3: 'Tuesday',
+      day_4: 'Wednesday',
+      day_5: 'Thursday',
+      day_6: 'Friday',
+      day_7: 'Saturday',
+    },
+  },
 };
--- src/i18n/fr/calendar.ts
+++ src/i18n/fr/calendar.ts
@@ -14,4 +14,31 @@ export default {
     month_12: 'Novembre',
     month_13: 'Décembre',
   },
+  tooltip: {
+    previousMonth: 'Mois précédent',
+    nextMonth: 'Mois suivant',
+  },
+  button: {
+    today: "Aujourd'hui",
+  },
+  days: {
+    short: {
+      day_1: 'Dim',
+      day_2: 'Lun',
+      day_3: 'Mar',
+      day_4: 'Mer',
+      day_5: 'Jeu',
+      day_6: 'Ven',
+      day_7: 'Sam',
+    },
+    long: {
+      day_1: 'Dimanche',
+      day_2: 'Lundi',
+      day_3: 'Mardi',
+      day_4: 'Mercredi',
+      day_5: 'Jeudi',
+      day_6: 'Vendredi',
+      day_7: 'Samedi',
+    },
+  },
 };
